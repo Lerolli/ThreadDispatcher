957f7fb0dfd205ccee8df67c978ff33b32ef6176
anton zakharov <zaharov.28097411@gmail.com>
2021-04-12 11:36:42 +0500

    Реализован ThreadedTask

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,15 @@
 public class Program {
     public static void main(String[] args){
 
-        var threadDispather = ThreadDispatcher.getInstance();
+        var threadDispatcher = ThreadDispatcher.getInstance();
         var consoleTask = new WriteOnConsoleWorker();
         var sleepTask = new SleepWorker();
         var infinityWorker = new InfinityWorker();
-        var thread1 = new Thread(infinityWorker);
-        var thread2 = new Thread(consoleTask);
-        threadDispather.Add(consoleTask);
-        threadDispather.Add(consoleTask);
-        threadDispather.Add(consoleTask);
-        threadDispather.Add(sleepTask);
-        threadDispather.AddInQueue(sleepTask);
+        threadDispatcher.Add(consoleTask);
+        threadDispatcher.Add(consoleTask);
+        threadDispatcher.Add(consoleTask);
+        threadDispatcher.Add(sleepTask);
+        threadDispatcher.Add(infinityWorker);
+        threadDispatcher.AddInQueue(sleepTask);
     };
 }
--- src/ThreadedTask.java
+++ src/ThreadedTask.java
@@ -1,4 +1,22 @@
+import java.util.ArrayDeque;
+
 public class ThreadedTask implements Runnable{
 
-    public void run(){};
+    public static ArrayDeque<ThreadedTask> activeTasks = new ArrayDeque<>();
+    private synchronized void updateActiveTasks() {
+        activeTasks = ThreadDispatcher.queue.clone();
+    }
+
+    public void run(){
+        while (true) {
+            try {
+                Thread.sleep(200);
+            }
+            catch (Exception e) {
+                System.out.println("Error in ThreadMonitor");
+            }
+            System.out.println(activeTasks);
+            updateActiveTasks();
+        }
+    };
 }
